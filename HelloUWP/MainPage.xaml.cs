55eebcfc9bf085a7cc364f9e893f6a31d24afd89
hiro <hiro0218@gmail.com>
2016-05-25 19:22:31 +0900

    通知機能実装の練習

--- HelloUWP/MainPage.xaml.cs
+++ HelloUWP/MainPage.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.System.Profile;
+using Windows.UI.Notifications;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using HelloUWP.Services;
+using System.Xml;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
 
@@ -27,6 +20,50 @@ namespace HelloUWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new MessageDialog("Dialog Message", "Dialog Title");
+            dlg.Commands.Add(new UICommand("はい", null, true));
+            dlg.Commands.Add(new UICommand("いいえ", null, false));
+            var selectedCommand = await dlg.ShowAsync();
+            bool result = (bool)selectedCommand.Id;
+
+            // 「はい」が選択された場合の動作
+            if (result)
+            {
+                // トーストを表示させる
+                templateNotify();
+            } else
+            {
+                string text = "Cancel";
+                string2Notify(text);
+            }
+        }
+
+        private void templateNotify()
+        {
+            var xmlDoc = ToastService.CreateToast();
+            var notifier = ToastNotificationManager.CreateToastNotifier();
+            var toast = new ToastNotification(xmlDoc);
+
+            notifier.Show(toast);
+        }
+
+        public static void string2Notify(String message)
+        {
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText01;
+            var toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+            var notifier = ToastNotificationManager.CreateToastNotifier();
+
+            // Set Text
+            var toastTextElements = toastXml.GetElementsByTagName("text");
+            toastTextElements[0].AppendChild(toastXml.CreateTextNode(message));
+
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            notifier.Show(toast);
         }
     }
 }
---
+++ HelloUWP/Services/ToastService.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+using Windows.Data.Xml.Dom;
+
+namespace HelloUWP.Services
+{
+    public static class ToastService
+    {
+        public static XmlDocument CreateToast()
+        {
+            var xDoc = new XDocument(
+                new XElement("toast",
+                    new XElement("visual",
+                        new XElement("binding", new XAttribute("template", "ToastGeneric"),
+                            new XElement("text", "To Do List"),
+                            new XElement("text", "Is the task complete?")
+                            ) // binding
+                        ), // visual
+                    new XElement("actions",
+                        new XElement("action", new XAttribute("activationType", "background"),
+                            new XAttribute("content", "Yes"), new XAttribute("arguments", "yes")),
+                        new XElement("action", new XAttribute("activationType", "background"),
+                            new XAttribute("content", "No"), new XAttribute("arguments", "no"))
+                        ) // actions
+                    )
+                );
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xDoc.ToString());
+            return xmlDoc;
+        }
+    }
+}
